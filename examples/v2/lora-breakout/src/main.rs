4cc44ce55fb05e5883a4bfee1a77d3b1cc59110c
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-01-08 10:09:21 +0100

    Support sending data
    Create API for setting config and sending data

--- examples/v2/lora-breakout/src/main.rs
+++ examples/v2/lora-breakout/src/main.rs
@@ -14,30 +14,27 @@ use rtt_target::rtt_init_print;
 
 use nrf52833_hal as hal;
 
+use core::pin::Pin;
 use drogue_rak811 as rak811;
 use hal::gpio::Level;
 use hal::pac::{TIMER0, UARTE0};
 use hal::timer::Timer;
 use hal::uarte::*;
 
-static LOGGER: RTTLogger = RTTLogger::new(LevelFilter::Debug);
+static LOGGER: RTTLogger = RTTLogger::new(LevelFilter::Trace);
 
 #[app(device = crate::hal::pac, peripherals = true)]
 const APP: () = {
     struct Resources {
         count: usize,
-        #[init([0; 1])]
-        tx_buf: [u8; 1],
-        #[init([0; 1])]
-        rx_buf: [u8; 1],
-        driver: rak811::Rak811Driver<UarteTx<'static, UARTE0>, UarteRx<'static, UARTE0>>,
+        driver: rak811::Rak811Driver<UarteTx<UARTE0>, UarteRx<UARTE0>>,
     }
 
-    #[init(resources = [tx_buf, rx_buf])]
+    #[init]
     fn init(ctx: init::Context) -> init::LateResources {
         rtt_init_print!();
         log::set_logger(&LOGGER).unwrap();
-        log::set_max_level(log::LevelFilter::Debug);
+        log::set_max_level(log::LevelFilter::Trace);
 
         let port0 = hal::gpio::p0::Parts::new(ctx.device.P0);
         let port1 = hal::gpio::p1::Parts::new(ctx.device.P1);
@@ -70,9 +67,7 @@ const APP: () = {
             Baudrate::BAUD115200,
         );
 
-        let (uarte_tx, uarte_rx) = uarte
-            .split(ctx.resources.tx_buf, ctx.resources.rx_buf)
-            .unwrap();
+        let (uarte_tx, uarte_rx) = uarte.split();
 
         let driver = rak811::Rak811Driver::new(uarte_tx, uarte_rx);
 
@@ -85,24 +80,47 @@ const APP: () = {
     fn idle(ctx: idle::Context) -> ! {
         let idle::Resources { driver } = ctx.resources;
 
-        send_command(driver, rak811::Command::QueryFirmwareInfo);
-        send_command(driver, rak811::Command::GetBand);
+        driver
+            .send_command(rak811::Command::QueryFirmwareInfo)
+            .map_err(|e| log::error!("ERROR: {:?}", e));
+        driver
+            .send_command(rak811::Command::GetBand)
+            .map_err(|e| log::error!("ERROR: {:?}", e));
+
+        driver
+            .set_mode(rak811::LoraMode::WAN)
+            .map_err(|e| log::error!("ERROR: {:?}", e));
+
+        driver
+            .set_device_address(&[0x00, 0x11, 0x22, 0x33])
+            .map_err(|e| log::error!("ERROR: {:?}", e));
+
+        driver
+            .set_device_eui(&[0x00, 0xBB, 0x7C, 0x95, 0xAD, 0xB5, 0x30, 0xB9])
+            .map_err(|e| log::error!("ERROR: {:?}", e));
+        driver
+            .set_app_eui(&[0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x03, 0xB1, 0x84])
+            .map_err(|e| log::error!("ERROR: {:?}", e));
+
+        driver
+            .set_app_key(&[
+                0xE2, 0xB5, 0x25, 0xB6, 0x86, 0xB8, 0xE2, 0xE6, 0xFE, 0x22, 0x27, 0x51, 0xAF, 0x35,
+                0xCD, 0x22,
+            ])
+            .map_err(|e| log::error!("ERROR: {:?}", e));
+
+        driver
+            .join(rak811::ConnectMode::OTAA)
+            .map_err(|e| log::error!("ERROR: {:?}", e));
+
+        driver
+            .send(rak811::QoS::Unconfirmed, 1, b"hello, world")
+            .map_err(|e| log::error!("ERROR: {:?}", e));
+
+        log::info!("Data sent!");
 
         loop {
             compiler_fence(Ordering::SeqCst);
         }
     }
 };
-
-fn send_command(
-    driver: &mut rak811::Rak811Driver<UarteTx<'static, UARTE0>, UarteRx<'static, UARTE0>>,
-    command: rak811::Command,
-) {
-    log::info!("Sending command: {:?}", command);
-    match driver.send(command) {
-        Ok(response) => log::info!("Response: {:?}", response),
-        Err(e) => {
-            log::info!("Command error: {:?}", e);
-        }
-    }
-}
